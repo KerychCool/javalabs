cd14f470e3dc4e067f3b924d808c458deaffa2dc
KerychCool <32357145+KerychCool@users.noreply.github.com>
2018-02-19 15:06:58 +0200

    Update Fura.java

--- lab1/Fura.java
+++ lab1/Fura.java
@@ -1,8 +1,20 @@
 package zlab1;
 
 public class Fura {
-	public double mvolume = 60.0;
-	public double mweight = 3500.0;
-	public double fuelconsump = 0.14;
-	public double fuelprice = 28; //0.00008485 btc
+	private double mvolume = 60.0;
+	private double mweight = 3500.0;
+	private double fuelconsump = 0.14;
+	private double fuelprice = 28; //0.00008485 btc
+        public double getMvolume(){
+            return mvolume;
+        }
+        public double getMweight(){
+            return mweight;
+        }
+        public double getFuelconsump(){
+            return fuelconsump;
+        }
+        public double getFuelprice(){
+            return fuelprice;
+        }
 }
